90a3f91485a4feb60c64cf09dd1ec5594405dc6b
etown285 <78112738+etown285@users.noreply.github.com>
2021-07-08 18:59:34 -0400

    Fixed model and routes connection with bookcontroller.js

--- models/book.js
+++ models/book.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.save);
+const bookSchema = new Schema({
+    id: {type: String, require: true},
+    title: {type: String, require: true},
+    authors: [{type: String, require: true}],
+    description: {type: String, require: true},
+    image: {type: String, unique: true, dropDups: true},
+    link: {type: String, required: true}
+})
 
+const GoogleBooks = mongoose.model("GoogleBooks", bookSchema);
 
-router.route("/:id")
-  .get(booksController.findOne)
-  .delete(booksController.remove);
-
-module.exports = router;
+module.exports = GoogleBooks;
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const booksController = require("../../controllers/booksController.js");
 
 router.route("/")
   .get(booksController.findAll)
